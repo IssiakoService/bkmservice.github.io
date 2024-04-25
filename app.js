4b94bb85ae4fe872bcdb5e3f44ffaa0cc6e0df5d
IssiakoService <154634142+IssiakoService@users.noreply.github.com>
2024-04-25 22:44:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+// Sélectionnez les éléments
+var passwordInput = document.getElementById("password");
+var rPasswordInput = document.getElementById("r_password");
+var passErrorDiv = document.getElementById("pass_error");
+var inscriptionButton = document.getElementById("inscription_bouton");
+
+// Fonction pour vérifier si les mots de passe correspondent
+function verifierMotsDePasse() {
+    var password = passwordInput.value;
+    var rPassword = rPasswordInput.value;
+
+    if (password === rPassword && password !== "" && rPassword !== "") {
+        // Les mots de passe correspondent
+        passErrorDiv.textContent = "";
+        inscriptionButton.disabled = false;
+    } else {
+        // Les mots de passe ne correspondent pas
+        passErrorDiv.textContent = "Les mots de passe ne correspondent pas.";
+        passErrorDiv.style.color = "red";
+        inscriptionButton.disabled = true;
+    }
+}
+
+// Ajoutez des écouteurs d'événements pour vérifier les mots de passe lors de la saisie
+passwordInput.addEventListener("input", verifierMotsDePasse);
+rPasswordInput.addEventListener("input", verifierMotsDePasse);
